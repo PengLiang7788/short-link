b5768ad67c6048e6fbf69adda33e9e475f467c5a
PengLiang7788 <1729631268@qq.com>
2023-01-10 11:22:42 +0800

    消费者链路开发

---
+++ link-shop/src/main/java/com/example/shortlink/shop/config/RabbitMQErrorConfig.java
@@ -0,0 +1,79 @@
+package com.example.shortlink.shop.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.MessageRecoverer;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 彭亮
+ * @create 2023-01-07 10:16
+ */
+@Configuration
+@Slf4j
+public class RabbitMQErrorConfig {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 异常交换机
+     */
+    private String orderErrorExchange = "order.error.exchange";
+
+    /**
+     * 异常队列
+     */
+    private String orderErrorQueue = "order.error.queue";
+
+    /**
+     * 异常routing key
+     */
+    private String orderErrorRoutingKey = "order.error.routing.key";
+
+    /**
+     * 创建异常交换机
+     */
+    @Bean
+    public TopicExchange errorTopicExchange() {
+        return new TopicExchange(orderErrorExchange, true, false);
+    }
+
+    /**
+     * 创建异常队列
+     * @return
+     */
+    @Bean
+    public Queue errorQueue() {
+        return new Queue(orderErrorQueue, true);
+    }
+
+    /**
+     * 建立绑定关系
+     * @return
+     */
+    @Bean
+    public Binding bindingErrorQueueAndExchange() {
+        return BindingBuilder.bind(errorQueue())
+                .to(errorTopicExchange())
+                .with(orderErrorRoutingKey);
+    }
+
+    /**
+     * 配置 MessageRecover
+     * 消费消息重试一定次数后，用特定的routingKey转发到指定的交换机中，方便后续排查和告警
+     * @return
+     */
+    @Bean
+    public MessageRecoverer messageRecoverer(){
+        return new RepublishMessageRecoverer(rabbitTemplate,orderErrorExchange,orderErrorRoutingKey);
+    }
+
+}
---
+++ link-shop/src/main/java/com/example/shortlink/shop/listener/ProductOrderMQListener.java
@@ -0,0 +1,41 @@
+package com.example.shortlink.shop.listener;
+
+import com.example.shortlink.common.enums.BizCodeEnum;
+import com.example.shortlink.common.exception.BizException;
+import com.example.shortlink.common.model.EventMessage;
+import com.example.shortlink.shop.service.ProductOrderService;
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 彭亮
+ * @create 2023-01-10 11:10
+ */
+@Configuration
+@Slf4j
+@RabbitListener(queuesToDeclare = {@Queue("order.close.queue")})
+public class ProductOrderMQListener {
+
+    @Autowired
+    private ProductOrderService productOrderService;
+
+    @RabbitHandler
+    public void productOrderHandler(EventMessage eventMessage, Message message, Channel channel){
+        log.info("监听到消息 ProductOrderMQListener message的消息内容:{}",message);
+        try{
+
+            //TODO
+
+        }catch (Exception e){
+            log.error("消费失败:{}",eventMessage);
+            throw new BizException(BizCodeEnum.MQ_CONSUME_EXCEPTION);
+        }
+        log.info("消费成功:{}",eventMessage);
+    }
+}
