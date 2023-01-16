6305c9080322b90f84e7e8a2c366cbeef9e96420
PengLiang7788 <1729631268@qq.com>
2023-01-16 15:54:37 +0800

    kafka工具类封装

--- link-app/src/main/java/com/example/shortlink/app/dwd/DwdShortLinkLogApp.java
+++ link-app/src/main/java/com/example/shortlink/app/dwd/DwdShortLinkLogApp.java
@@ -1,8 +1,10 @@
 package com.example.shortlink.app.dwd;
 
+import com.example.shortlink.app.util.KafkaUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
 /**
  * @author 彭亮
@@ -11,13 +13,25 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 @Slf4j
 public class DwdShortLinkLogApp {
 
+    /**
+     * 定义topic
+     */
+    public static final String SOURCE_TOPIC = "ods_link_visit_topic";
+
+    /**
+     * 定义消费者组
+     */
+    private static final String GROUP_ID = "dwd_short_link_group";
+
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         env.setParallelism(1);
 
-        DataStreamSource<String> ds = env.socketTextStream("192.168.200.140", 8888);
+        FlinkKafkaConsumer<String> kafkaConsumer = KafkaUtil.getKafkaConsumer(SOURCE_TOPIC, GROUP_ID);
+
+        DataStreamSource<String> ds = env.addSource(kafkaConsumer);
 
         ds.print();
 
---
+++ link-app/src/main/java/com/example/shortlink/app/util/KafkaUtil.java
@@ -0,0 +1,66 @@
+package com.example.shortlink.app.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author 彭亮
+ * @create 2023-01-16 15:25
+ */
+@Slf4j
+public class KafkaUtil {
+
+    /**
+     * kafka的broker地址
+     */
+    private static String KAFKA_SERVER = null;
+
+    static {
+        Properties properties = new Properties();
+
+        InputStream is = KafkaUtil.class.getClassLoader().getResourceAsStream("application.properties");
+
+        try {
+            properties.load(is);
+
+        } catch (IOException e) {
+            log.error("加载kafka配置文件失败");
+        }
+
+        //获取key配置对应的value
+        KAFKA_SERVER = properties.getProperty("kafka.server");
+    }
+
+    /**
+     * 获取flink的kafka消费者
+     *
+     * @param topic
+     * @param groupId
+     * @return
+     */
+    public static FlinkKafkaConsumer<String> getKafkaConsumer(String topic, String groupId) {
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+
+        return new FlinkKafkaConsumer<String>(topic, new SimpleStringSchema(), properties);
+    }
+
+    /**
+     * 获取flink的kafka生产者
+     *
+     * @param topic
+     * @return
+     */
+    public static FlinkKafkaProducer<String> getKafkaProducer(String topic) {
+        return new FlinkKafkaProducer<String>(KAFKA_SERVER, topic, new SimpleStringSchema());
+    }
+}
