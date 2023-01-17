1134ec6ab3321a0f065e98e3a422affbbe61a336
PengLiang7788 <1729631268@qq.com>
2023-01-17 16:59:22 +0800

    识别新老用户写入dwd层

--- link-app/src/main/java/com/example/shortlink/app/dwd/DwdShortLinkLogApp.java
+++ link-app/src/main/java/com/example/shortlink/app/dwd/DwdShortLinkLogApp.java
@@ -2,15 +2,20 @@ package com.example.shortlink.app.dwd;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.example.shortlink.app.func.VisitorMapFunction;
 import com.example.shortlink.app.util.DeviceUtil;
 import com.example.shortlink.app.util.KafkaUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.util.Collector;
 
 import java.net.URL;
@@ -25,9 +30,14 @@ import java.util.TreeMap;
 public class DwdShortLinkLogApp {
 
     /**
-     * 定义topic
+     * 定义source topic
      */
     public static final String SOURCE_TOPIC = "ods_link_visit_topic";
+
+    /**
+     * 定义sink topic
+     */
+    public static final String SINK_TOPIC = "dwd_link_visit_topic";
 
     /**
      * 定义消费者组
@@ -40,9 +50,11 @@ public class DwdShortLinkLogApp {
 
         env.setParallelism(1);
 
-        FlinkKafkaConsumer<String> kafkaConsumer = KafkaUtil.getKafkaConsumer(SOURCE_TOPIC, GROUP_ID);
+        DataStream<String> ds =  env.socketTextStream("192.168.200.140",8888);
 
-        DataStreamSource<String> ds = env.addSource(kafkaConsumer);
+//        FlinkKafkaConsumer<String> kafkaConsumer = KafkaUtil.getKafkaConsumer(SOURCE_TOPIC, GROUP_ID);
+
+//        DataStreamSource<String> ds = env.addSource(kafkaConsumer);
 
         ds.print();
 
@@ -60,6 +72,23 @@ public class DwdShortLinkLogApp {
                 out.collect(jsonObject);
             }
         });
+
+        // 分组
+        KeyedStream<JSONObject, String> keyedStream = jsonDS.keyBy(new KeySelector<JSONObject, String>() {
+            @Override
+            public String getKey(JSONObject value) throws Exception {
+                return value.getString("udid");
+            }
+        });
+
+        // 识别 richMap函数，做状态存储的初始化
+        SingleOutputStreamOperator<String> jsonDSWithVisitorState = keyedStream.map(new VisitorMapFunction());
+        jsonDSWithVisitorState.print("ods新老访客");
+
+        // 存储到dwd
+        FlinkKafkaProducer<String> kafkaProducer = KafkaUtil.getKafkaProducer(SINK_TOPIC);
+        jsonDSWithVisitorState.addSink(kafkaProducer);
+
         env.execute();
     }
 
