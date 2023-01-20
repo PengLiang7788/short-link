f788c956ce62edfc67da5dd0bbab6b2fecc9bde4
PengLiang7788 <1729631268@qq.com>
2023-01-20 10:49:36 +0800

    gateway模块创建

---
+++ link-gateway/src/main/java/com/example/shortlink/gateway/GatewayServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 彭亮
+ * @create 2023-01-20 10:46
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
